78073cbaeaa87eaed77ec18c640d50e6671a7b14
Jitae Kim <originerd@gmail.com>
2015-03-27 18:21:44 +0900

    Added _google.html.erb

--- app/helpers/googles_helper.rb
+++ app/helpers/googles_helper.rb
@@ -1,2 +1,17 @@
 module GooglesHelper
+
+  def to_table_tag(content)
+    result = "<table><tbody>"
+
+    lines = content.split("\n")
+    lines.each do |line|
+      result += "<tr>"
+      line.split.each { |word| result += "<td>#{word}</td>" }
+      result += "</tr>"
+    end
+
+    result += "</tbody></table>"
+
+    result
+  end
 end
