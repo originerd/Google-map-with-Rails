345bbe828bddba8dc4d6e419014deb1e8973bb02
Jitae Kim <originerd@gmail.com>
2015-03-29 22:22:44 +0900

    commit

--- app/helpers/googles_helper.rb
+++ app/helpers/googles_helper.rb
@@ -1,7 +1,9 @@
 module GooglesHelper
 
+  require 'open-uri'
+
   def to_table_tag(google)
-    result = "<h2>#{google.latlng}</h2><h3>#{google.address}</h3><table class=\"table table-striped\"><tbody><tr><th>채널(고정값)</th><th>대역폭(고정값)</th><th>측정결과</th></tr>"
+    result = "<h2>#{google.latlng}</h2><h3>#{get_address_via(google.latlng)}</h3><table class=\"table table-striped\"><tbody><tr><th>채널(고정값)</th><th>대역폭(고정값)</th><th>측정결과</th></tr>"
 
     lines = google.content.split("\n")
     lines.each do |line|
@@ -13,5 +15,13 @@ module GooglesHelper
     result += "</tbody></table>"
 
     result
+  end
+
+  def get_address_via(latlng)
+    latlng.delete!(' ')
+
+    results = JSON.load(open("http://maps.googleapis.com/maps/api/geocode/json?language=ko&latlng=#{latlng}"))
+
+    results["results"][0]["formatted_address"]
   end
 end
---
+++ db/migrate/20150329132236_remove_address_from_googles.rb
@@ -0,0 +1,4 @@
+class RemoveAddressFromGoogles < ActiveRecord::Migration
+  def change
+  end
+end
